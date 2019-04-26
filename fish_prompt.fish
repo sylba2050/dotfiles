d0871fb03b504b0909cd90aa3966c5228ab0a6a6
masataka.hisasue <masataka.hisasue@optim.co.jp>
2019-04-26 13:09:37 +0900

    feat: view pyenv status

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,3 +1,7 @@
 function fish_prompt --description 'Write out the prompt'
-	printf '%s %s %s %s' (date "+%H:%M:%S") (set_color yellow)(echo $USER) (set_color cyan)(prompt_pwd) (set_color red)(echo "> ")
+    if test -n "$VIRTUAL_ENV"
+        printf '(%s) %s %s %s' (basename $VIRTUAL_ENV) (set_color yellow)(echo $USER) (set_color cyan)(prompt_pwd) (set_color red)(echo "> ")
+    else
+        printf '%s %s %s %s' (date "+%H:%M:%S") (set_color yellow)(echo $USER) (set_color cyan)(prompt_pwd) (set_color red)(echo "> ")
+    end
 end
