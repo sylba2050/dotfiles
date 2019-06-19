e501643977884fd8ebcc40400fe3900dc6a9d593
Masataka Hisasue <masataka.hisasue@optim.co.jp>
2019-06-19 11:40:18 +0900

    feat: language code in less

--- config.fish
+++ config.fish
@@ -5,5 +5,7 @@ eval (pyenv virtualenv-init - | source)
 set -x GOPATH $HOME/go
 set -x PATH $PATH $GOPATH/bin $HOME/.cargo/env $PYENV_ROOT/bin
 
+set -x LESSCHARSET utf-8
+
 source ~/.config/fish/function/fish_prompt.fish
 source ~/.fish_alias
