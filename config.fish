c9815aa545d51ea8ef2a69f0228153b301112864
Masataka Hisasue <masataka.hisasue@optim.co.jp>
2021-01-05 08:38:36 +0900

    Feat use nodebrew

--- config.fish
+++ config.fish
@@ -1,11 +1,8 @@
-set -x PYENV_ROOT $HOME/.pyenv
-eval (pyenv init - | source)
-eval (pyenv virtualenv-init - | source)
-
 set -x GOPATH $HOME/go
 set -x PATH $PATH $GOPATH/bin $HOME/.cargo/env $PYENV_ROOT/bin
+set -x PATH $PATH $HOME/.nodebrew/current/bin
 
 set -x LESSCHARSET utf-8
 
-source ~/.config/fish/function/fish_prompt.fish
+source ~/.config/fish/functions/fish_prompt.fish
 source ~/.fish_alias
